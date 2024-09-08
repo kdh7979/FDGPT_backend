49e560de73fbbc88fd6c0b5b0d9da1c93b1c6efd
kdh7979 <kimdohyeon0506@gmail.com>
2024-09-08 21:54:15 +0900

    chatRoom 생성 에러

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, json, request, g, render_template
+from flask import Flask, json, request, g, render_template, redirect, url_for
 
 from src.middleware.cors import cors
 from src.database.database import init_db
@@ -7,6 +7,8 @@ from config import config
 from src.database.controller import create_chat, create_chat_room
 
 from flask_socketio import SocketIO, join_room, emit
+
+import uuid
 
 
 def init_app():
@@ -25,7 +27,17 @@ engine, get_db = init_db()
 @socketio.on('message')
 def handle_message(data):
     print('Received message:', data)
+    create_chat(get_db(), chat=data['message'], room_id=data['room_id'])
     emit('message', data)
+
+@socketio.on('join')
+def handle_join(data):
+    room_id = str(uuid.uuid4())
+    print("received data to join:", data, room_id)
+    print(f"SID: {request.sid} is trying to join room {room_id}")
+    join_room(room_id)
+    create_chat_room(get_db(), room_id=room_id, room_name=data['roomName'])
+    return redirect(url_for("chatting", room_id=room_id))
 
 
 
@@ -40,9 +52,9 @@ def teardown_db(exception):
 def index():
     return render_template('index.html')
 
-@app.route('/chat')
-def chatting():
-    return render_template('chat.html')
+@app.route('/chat/<room_id>')
+def chatting(room_id):
+    return render_template('chat.html', room_id=room_id)
 
 @app.route('/status')
 def status():
